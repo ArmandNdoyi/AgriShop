b75338b86750309d1b5593bfa131b8b7f13826d9
Armand Ndoyi <choarmand96@gmail.com>
2023-06-15 14:56:03 +0100

    OnBoarding Screen Successfully added User can now remain Login even after application is closed Onboarding screen only appears for the 1st time using the application

--- app/src/main/java/com/example/agrishop/RegistrationActivity.java
+++ app/src/main/java/com/example/agrishop/RegistrationActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -20,22 +21,39 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText name, email, password;
     private FirebaseAuth auth;
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
         getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
 
-       /* if (auth.getCurrentUser() != null) {
+       if (auth.getCurrentUser() != null) {
             startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
             finish();
-        } */
+        }
 
-        auth = FirebaseAuth.getInstance();
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+
+        boolean isFirstTime = sharedPreferences.getBoolean("firstTime", true);
+
+        if (isFirstTime){
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("firstTime",false);
+            editor.commit();
+
+            Intent intent = new Intent(RegistrationActivity.this,OnBoardingActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void signup (View view) {
